a7f1034d6e98f59fc7884ead4322d6dd31bb3c47
floatingatoll <rsoderberg@gmail.com>
2013-08-23 13:47:30 -0700

    skip this 80% of the time

--- lib/python/hackabot/plugins/ponies.py
+++ lib/python/hackabot/plugins/ponies.py
@@ -8,12 +8,16 @@ smaller-than-average equine creatures comes up.
 from zope.interface import implements
 from twisted.plugin import IPlugin
 from hackabot.plugin import IHackabotPlugin
+from random import randrange
 
 class Ponies(object):
     implements(IPlugin, IHackabotPlugin)
 
     @staticmethod
     def msg(conn, event):
+        if randrange(1, 100) <= 80:
+            return
+
         if event["sent_by"] == conn.nickname:
             return
 
